a39ce58d0b71af1ad6de6e777e2a33198aad61e6
Ashish S Sharma <ashishsanjaysharma@gmail.com>
2017-11-25 10:21:31 +0530

    Initial commit

---
+++ servo using pwm.c
@@ -0,0 +1,84 @@
+
+#define F_CPU 14745600
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+
+void servo1_pin_config (void)
+{
+ DDRB  = DDRB | 0x20;  //making PORTB 5 pin output
+ PORTB = PORTB | 0x20; //setting PORTB 5 pin to logic 1
+}
+
+void servo2_pin_config (void)
+{
+ DDRB  = DDRB | 0x40;  //making PORTB 6 pin output
+ PORTB = PORTB | 0x40; //setting PORTB 6 pin to logic 1
+}
+
+void port_init(void)
+{
+ servo1_pin_config(); //Configure PORTB 5 pin for servo motor 1 operation
+ servo2_pin_config(); //Configure PORTB 6 pin for servo motor 2 operation
+}
+
+void timer1_init(void)
+{
+ TCCR1B = 0x00; //stop
+ TCNT1H = 0xFC; //Counter high value to which OCR1xH value is to be compared with
+ TCNT1L = 0x01;	//Counter low value to which OCR1xH value is to be compared with
+ OCR1AH = 0x03;	//Output compare Register high value for servo 1
+ OCR1AL = 0xFF;	//Output Compare Register low Value For servo 1
+ OCR1BH = 0x03;	//Output compare Register high value for servo 2
+ OCR1BL = 0xFF;	//Output Compare Register low Value For servo 2
+ OCR1CH = 0x03;	//Output compare Register high value for servo 3
+ OCR1CL = 0xFF;	//Output Compare Register low Value For servo 3
+ ICR1H  = 0x03;	
+ ICR1L  = 0xFF;
+ TCCR1A = 0xAB; 
+ TCCR1C = 0x00;
+ TCCR1B = 0x0C; //WGM12=1; CS12=1, CS11=0, CS10=0 (Prescaler=256)
+}
+
+void init_devices(void)
+{
+ cli(); //disable all interrupts
+ port_init();
+ timer1_init();
+ sei(); //re-enable interrupts 
+}
+
+//Function to rotate Servo 1 by a specified angle in the multiples of 1.86 degrees
+void servo_1(unsigned char degrees)  
+{
+ float PositionPanServo = 0;
+  PositionPanServo = ((float)degrees / 1.86) + 35.0;
+ OCR1AH = 0x00;
+ OCR1AL = (unsigned char) PositionPanServo;
+}
+
+//Function to rotate Servo 2 by a specified angle in the multiples of 1.86 degrees
+void servo_2(unsigned char degrees)
+{
+ float PositionTiltServo = 0;
+ PositionTiltServo = ((float)degrees / 1.86) + 35.0;
+ OCR1BH = 0x00;
+ OCR1BL = (unsigned char) PositionTiltServo;
+}
+
+//Main function
+void main_servo()
+{
+ unsigned char i = 0;
+ init_devices();
+ 	
+ 	for (i = 0; i <90; i++)
+ 	{
+  		servo_1(i);
+		  _delay_ms(30);
+  		servo_2(i);
+ 		 _delay_ms(30);
+	 }
+
+}
